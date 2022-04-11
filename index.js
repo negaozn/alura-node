a2aa94d616b2022c65800310fc07e87182ba593b
eduardo.zn <eduardo.zn@gmail.com>
2022-04-11 01:48:33 -0300

    Função lerArquivo Adicionada

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const fs = require('fs')
+import chalk from 'chalk'
+import fs from 'fs'
+
+function lerArquivo(path){
+    const encoding = 'utf-8'
+    fs.readFile(path,encoding,function(err,data){
+        if (err){
+            console.log(chalk.red('Algo deu errado: ', err))
+        }
+        console.log(chalk.green(data))
+    })
+}
+
+/*function texto(string) {
+  return string;
+}
+
+console.log(texto(paragrafo));*/
+
+lerArquivo('./arquivos/texto1.md')
